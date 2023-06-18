9bd55a158dcedee7321e69f1dd942c4eb1c9beb4
Ivan Stupnitskiy <40752497+Rukatonoshi@users.noreply.github.com>
2023-06-18 22:17:23 +0300

    Update Prepared_csv.py
    updated ver

--- Prepared_csv.py
+++ Prepared_csv.py
@@ -1,7 +1,8 @@
 import numpy as np
 import pandas as pd
-
 import os
+from sklearn.preprocessing import LabelEncoder
+
 dir_path = os.path.dirname(os.path.realpath(__file__))
 file = dir_path + '//Dataset-Unicauca-Version2-87Atts.csv'
 
@@ -36,6 +37,25 @@ data.reset_index(drop=True, inplace=True)
 
 # remove columns which contains zeroes in the data
 data = data.loc[:, (data != 0).any(axis=0)]
+print(data.shape)
+
+print(data['ProtocolName'])
+
+# converting the protocol name (target column) to required format (int)
+# using LabelEncoder function from sklearn.preprocession library
+encoder = LabelEncoder().fit(data['ProtocolName'])
+data['ProtocolName'] = encoder.transform(data['ProtocolName'])
+values = encoder.inverse_transform(data['ProtocolName'])
+target_column = data['ProtocolName']
+
+# mapping the encoded value
+encoded_target_column = {}
+for i in range(len(data['ProtocolName'])):
+    encoded_target_column[data['ProtocolName'][i]] = values[i]
+print(encoded_target_column)
+
+dataset = data.drop(['Flow.ID','Source.IP','Label', 'Timestamp','Destination.IP', 'Source.Port', 'Destination.Port', 'Protocol'], axis=1)
+print(dataset.shape)
 
 # write rendered dataframe to csv file
-data.to_csv(dir_path+'//rendered_dataset.csv')
+dataset.to_csv(dir_path+'//rendered_dataset.csv')
